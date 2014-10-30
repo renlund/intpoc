9e39045ae61267b6cef2a8b1a1860968592bbf23
Henrik <henrik.renlund@ucr.uu.se>
2014-10-30 15:33:52 +0100

    main function 'win_mean' ready to be implemented

--- R/win_mean.r
+++ R/win_mean.r
@@ -4,27 +4,150 @@
 #' @param y measurements
 #' @param x. point at which to calculate mean
 #' @param win window over whitch to calculate mean
+#' @param check apply checking function \code{check_num_vec} to \code{x} and \code{y}?
+#' @param mess messages on?
+#' @importFrom dplyr data_frame
 #' @export
 
-win_mean <- function(x, y, x., win){
+win_mean <- function(x, y, x., win, check=TRUE, mess=FALSE){
+    y_copy <- na.omit(y)
+    if(check) {
+        if(typeof(df <- check_num_vec(x, y, pre_mess = "[win_mean]", mess=mess))!="logical"){
+            if(!is.null(df)){
+                x <- df$x
+                y <- df$y
+            }
+        } else {
+            stop("[win_mean] num_check_vec failed")
+        }
+    }
+    if(!(missing(x.) & missing(win))){
+        if(missing(x.))  x.  <- x[length(x)]
+        if(missing(win)) win <- diff(range(x))
+        if(typeof(df <- windowize(x, y, x., win,
+                                  check=FALSE,
+                                  pre_mess="[win_mean]",
+                                  mess=FALSE)) != "logical"){
+            if(!is.null(df)){
+                x <- df$x
+                y <- df$y
+            }
+        } else {
+            stop("[win_mean] windowize fail" )
+        }
+    }
+    data_frame(
+        int = sum(vecrea(x, y, check=FALSE)),
+        sq_int = sum(vecre(x, y, check=FALSE)),
+        mean = int / diff(range(x)),
+        sd = sq_int / diff(range(x)) - (mean)^2,
+        raw_mean = mean(y_copy),
+        raw_sd = sd(y_copy)
+    )
+}
+
+#' @importFrom dplyr data_frame
+
+check_num_vec <- function(x, y, pre_mess="", mess=TRUE){
+    changes_made <- FALSE
+    if(any(is.na(x))) {
+        warning(paste(pre_mess, "no NA:s in x, please"))
+        return(invisible(NA))
+    }
     n <- length(x)
-    if(length(y) != x) stop("[win_mean] x, y lengths differ")
-    if(any(is.na(x))) stop("[win_mean] no NA:s in x, please")
-    o_x <- order(x)
-    if(!identical(o_x,1:n)){
-        message("[win_mean] x wasn't ordered")
-        y <- y[o_x]
-        x <- x[o_x]
+    if(length(y) != n) {
+        warning(paste(pre_mess, "x, y lengths differ"))
+        return(invisible(NA))
+    }
+    if(any(diff(sort(x))==0)){
+        if(mess) message(paste(pre_mess, "for non-unique x's the corresponding mean of y is used"))
+        y <- as.numeric(tapply(X = y, INDEX = x, FUN = mean, na.rm=TRUE, simplify = TRUE))
+        x <- sort(unique(x))
+        n <- length(x)
+        changes_made <- TRUE
+    } else {
+        o_x <- order(x)
+        if(!identical(o_x,1:n)){
+            if(mess) message(paste(pre_mess, "x wasn't ordered"))
+            y <- y[o_x]
+            x <- x[o_x]
+            changes_made <- TRUE
+        }
+    }
+    if(n<2) {
+        warning(paste(pre_mess), "(unique) x needs to be of length > 1")
+        return(invisible(NA))
+    }
+    y_NA <- is.na(y)
+    if(all(y_NA)) {
+        warning(paste(pre_mess, "y is too NA-infested"))
+        return(invisible(NA))
     }
     if(any(is.na(y))){
         y <- linRepNA(x, y, F, F)
-        message("[win_mean] 'y' had NA:s which were interpolated")
+        changes_made <- TRUE
+        if(mess) message(paste(pre_mess, "'y' had NA:s which were interpolated"))
     }
+    if(changes_made) data_frame(x=x, y=y) else invisible(NULL)
+}
+
+#' @importFrom dplyr data_frame
+
+windowize <- function(x, y, x., win, check=TRUE, pre_mess = "", mess=FALSE){
+    changes_made <- FALSE
+    if(check) {
+        if(typeof(df <- check_num_vec(x, y, pre_mess = "[windowize]", mess=mess))!="logical"){
+            if(!is.null(df)){
+                x <- df$x
+                y <- df$y
+            }
+        } else {
+            if(mess) warning("[windowize] num_check_vec failed")
+            return(invisible(NA))
+        }
+    }
+    n <- length(x)
     if(missing(x.)) {
         x. <- x[n]
-        message("[win_mean] latest timepoint used as 'x.'")
+        if(mess) message(paste(pre_mess, "latest timepoint used as 'x.'"))
     }
-    "woooha!"
+    if(x.>x[n] | x.<x[1]) {
+        if(mess) warning(paste(pre_mess, "'x.' out of range (of 'x')"))
+        return(invisible(NA))
+    } else {
+        if(x. %in% x){
+            if(x.!=x[n]){
+                y <- y[x<=x.]
+                x <- x[x<=x.]
+                changes_made <- TRUE
+            }
+        } else {
+            i <- sum(x<x.)
+            L <- line(x[i:(i+1)], y[i:(i+1)], check = FALSE)
+            y_new <- unname(L['intersect'] + L['slope']*x.)
+            x <- c(x[1:i], x.)
+            y <- c(y[1:i], y_new)
+            changes_made <- TRUE
+        }
+    }
+    if(missing(win)){
+        win <- diff(range(x))
+        if(mess) message(paste(pre_mess, "span x used as 'win'"))
+    } else {
+        low <- x.-win
+        if(low %in% x | low < x[1]){
+            y <- y[x>=low]
+            x <- x[x>=low]
+        } else {
+            i <- sum(x<low)
+            L <- line(x[i:(i+1)], y[i:(i+1)], check = FALSE)
+            y_new <- unname(L['intersect'] + L['slope']*low)
+            x <- c(low, x[(i+1):n])
+            y <- c(y_new, y[(i+1):n])
+        }
+        changes_made <- TRUE
+    }
+    if(changes_made) data_frame(x=x, y=y) else invisible(NULL)
 }
 
 check_num_pair <- function(x, y, pre_mess=""){
@@ -35,8 +158,8 @@ check_num_pair <- function(x, y, pre_mess=""){
     invisible(NULL)
 }
 
-line <- function(x,y){
-    check_num_pair(x,y, "[line]")
+line <- function(x, y, check=TRUE){
+    if(check) check_num_pair(x,y, "[line]")
     a <- if(x[1]==0)
         y[1]
     else if(x[2]==0)
@@ -47,26 +170,75 @@ line <- function(x,y){
     c("intersect"=a, "slope"=b)
 }
 
-h_cut <- function(x, y, h, between=TRUE){
+h_cut <- function(x, y, h, between=TRUE, check=TRUE){
     check_num_pair(x, y, "[h_cut]")
     isit <- min(y) <= h & max(y) >= h
     if(between & !isit) return(NULL)
-    L <- line(x, y)
+    L <- line(x, y, check=FALSE)
     if(L['slope']==0) return(NULL)
-    as.numeric((h-L['intersect']) / L['slope'])
+    unname((h-L['intersect']) / L['slope'])
 }
 
-pairea <- function(x, y){
-    check_num_pair(x, y, "[pairea]")
-    base <- diff(x)
-    if(diff(y)==0){
-        return( base * y[1] )
-    } else if(min(y)>=0 | max(y)<=0){
-        return( base * sum(y) / 2 )
-    } else {
-        x. <- h_cut(x, y, h=0, between=TRUE)
-        s1 <- pairea(x=c(x[1], x.), y=c(y[1], 0))
-        s2 <- pairea(x=c(x., x[2]), y=c(0, y[2]))
-        return( s1 + s2)
-    }
+# pairea2 <- function(x, y, check=TRUE){
+#     if(check) check_num_pair(x, y, "[pairea]")
+#     base <- diff(x)
+#     if(diff(y)==0){
+#         return( base * y[1] )
+#     } else if(min(y)>=0 | max(y)<=0){
+#         return( base * sum(y) / 2 )
+#     } else {
+#         x. <- h_cut(x, y, h=0, between=TRUE, check=FALSE)
+#         s1 <- pairea(x=c(x[1], x.), y=c(y[1], 0), check=FALSE)
+#         s2 <- pairea(x=c(x., x[2]), y=c(0, y[2]), check=FALSE)
+#         return( s1 + s2 )
+#     }
+# }
+
+pairea <-function(x, y, check=TRUE){
+    if(check) check_num_pair(x, y, "[pairea]")
+    0.5 * diff(x) * sum(y)
 }
+
+vecrea <- function(x, y, check=TRUE){
+    if(check) {
+        if(!is.null(df <- check_num_vec(x, y, pre_mess = "[vecrea]"))){
+            if(!"logical" %in% class(df)){
+                x <- df$x
+                y <- df$y
+            }
+        }
+    }
+    m <- length(x)-1
+    s <- rep(NA_real_, m)
+    for(k in 1:m){
+        s[k] <- pairea(x[k:(k+1)], y[k:(k+1)], check=FALSE)
+    }
+    s
+}
+
+paire <- function(x, y, check=TRUE){
+    if(check) check_num_pair(x, y, "[pairsq]")
+    L <- line(x, y, check=FALSE)
+    a <- unname(L['intersect'])
+    b <- unname(L['slope'])
+    a^2*diff(x) + a*b*diff(x^2) + b^2*diff(x^3) / 3
+}
+
+vecre <- function(x, y, check=TRUE){
+    if(check) {
+        if(!is.null(df <- check_num_vec(x, y, pre_mess = "[vecre]"))){
+            if(!"logical" %in% class(df)){
+                x <- df$x
+                y <- df$y
+            }
+        }
+    }
+    m <- length(x)-1
+    s <- rep(NA_real_, m)
+    for(k in 1:m){
+        s[k] <- paire(x[k:(k+1)], y[k:(k+1)], check=FALSE)
+    }
+    s
+}
+
+
--- inst/tests/test_win_mean_and_helpers.r
+++ inst/tests/test_win_mean_and_helpers.r
@@ -1,8 +1,31 @@
-context("'win_mean' and helper functions")
+context("'win_mean' and helper functions are ok")
 
 test_that("'check_num_pair' works", {
     expect_null(check_num_pair(1:2, 2:1))
     expect_null(check_num_pair(0:1, c(-1, 1)))
+})
+test_that("'check_num_vec' works", {
+    expect_warning(check_num_vec(1, 1))
+    expect_warning(check_num_vec(1, 1:2))
+    expect_warning(check_num_vec(c(1, NA), 1:2))
+    expect_warning(check_num_vec(1:2, c(NA, NA)))
+    expect_warning(check_num_vec(1:1, c(NA, NA)))
+    expect_message(expect_equal(
+        check_num_vec(c(2,3,1), c(1,2,3)),
+        data_frame(x=c(1,2,3), y=c(3,1,2))
+        ))
+    expect_message(expect_equal(
+        check_num_vec(c(2,3,1), c(1,2,NA)),
+        data_frame(x=c(1,2,3), y=c(1,1,2))
+    ))
+    expect_message(expect_equal(
+        check_num_vec(x = c(2,3,2), y = c(1,2,NA)),
+        data_frame(x=c(2,3), y=c(1,2))
+    ))
+    expect_message(expect_equal(
+        check_num_vec(c(1,1,2,4), c(1,3,NA,5)),
+        data_frame(x=c(1,2,4), y=c(2,3,5))
+    ))
 })
 test_that("'line' works", {
     expect_equal(line(c(1,2), c(1, 5)), c("intersect"=-3, "slope"=4))
@@ -17,3 +40,64 @@ test_that("'pairea' works", {
     expect_equal(pairea(0:1, c(0,20)), 10)
     expect_equal(pairea(c(0,2), c(-20,10)), -10)
 })
+test_that("'vecrea' works", {
+    expect_equal(vecrea(c(1,3,5,7), c(0,-1,1,3)), c(-1, 0, 4))
+    expect_equal(vecrea(c(1,3,5,7), c(0,-1,1,3)), c(-1, 0, 4))
+})
+test_that("'paire' works", {
+    expect_equal(paire(0:1, 0:1),1/3)
+    expect_equal(paire(0:1, c(1,1)),1)
+    expect_equal(paire(0:1, c(2,2)),4)
+    expect_equal(paire(x = 0:1, y = c(-1,1)),1/3)
+    expect_equal(paire(x = c(-2,3), y = c(-7,2)), 65)
+    expect_equal(paire(x = c(-2,3), y = c(-7,-5)), 181+2/3)
+})
+test_that("'vecre' works", {
+    expect_equal(vecre(x=c(4,5,5.5),y=c(0,1,1.5)), c(1/3, 19/24))
+})
+test_that("'win_mean' basic functionality works", {
+    expect_equal( win_mean( x = c(0, 1), y = c(1, 1))$int, 1)
+    expect_equal( win_mean(c(1,3,5,7), c(0,-1,1,3))$int, 3)
+    expect_equal( win_mean(c(1,3,5,7), c(NA,-1,1,3))$int, 2)
+    expect_equal( win_mean(x = c(1,3,5,7), y = c(0,-1,NA,3))$int, 3)
+    expect_equal( win_mean(c(1,3,5,7), c(0,-1,1,NA))$int, 1)
+})
+test_that("'win_mean' argument 'x.'  works", {
+    expect_equal( win_mean( x = c(0, 1), y = c(1, 1), x.=0.85)$int, 0.85)
+    expect_equal( win_mean(c(0, 1), c(1, 1), x.=0.5)$int, 0.5)
+    expect_equal( win_mean(c(0, 1), c(1, 1), x.=0.05)$int, 0.05)
+    expect_equal( win_mean(c(1,3,5,7), c(0,-1,1,3), x.=7)$int, 3)
+    expect_equal( win_mean(c(1,3,5,7), c(0,-1,1,3), x.=6)$int, 0.5)
+    expect_equal( win_mean(c(1,3,5,7), c(NA,-1,1,3), x.=6)$int, -0.5)
+    expect_error( win_mean(x = c(1,3,5,7), y = c(0,-1,1,3)$int, x.=8))
+    expect_error( win_mean(c(1,3,5,7), c(0,-1,1,3), x.=0)$int)
+})
+test_that("'win_mean' argument 'win'  works", {
+    expect_equal( win_mean(c(0, 1), c(1, 1), win=0.81)$int, 0.81)
+    expect_equal( win_mean(c(0, 1), c(1, 1), win=0.5)$int, 0.5)
+    expect_equal( win_mean(c(0, 1), c(1, 1), win=0.123)$int, 0.123)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=1)$int, 2.5)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=4)$int, 4)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=3)$int, 4.5)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=5)$int, 3.25)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=6)$int, 3)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=16)$int, 3)
+})
+test_that("'win_mean' argument 'win' works with 'x.' works", {
+    expect_equal( win_mean(c(0, 1), c(1, 1), win=0.81, x.=0.9)$int, 0.81)
+    expect_equal( win_mean(c(0, 1), c(1, 1), win=0.81, x.=0.6)$int, 0.6)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=1, x.=6)$int, 1.5)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=2, x.=5)$int, 0)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=1.5, x.=5.5)$int, 1.125)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=0.5, x.=3.5)$int, -0.375)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=2.5, x.=3.5)$int, -1.375)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=2.51, x.=3.5)$int, -1.375)
+    expect_equal(win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=Inf, x.=3.5)$int, -1.375)
+})
+# test_that("'win_mean' works for some test cases", {
+#     expect_equal(
+#         win_mean(x=c(1,3,5,7), y=c(0,-1,1,3), win=1.5, x.=5.5),
+#         data_frame(int=9/8, mean=3/4, sq_int=9/8, sd=3/16)
+#     )
+# })
+
