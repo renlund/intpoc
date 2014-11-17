13d466ced686c1e85aae3a19f92ecad495e36638
Henrik <henrik.renlund@ucr.uu.se>
2014-11-17 10:18:29 +0100

    added 'trans_sd_fnc' as a variation on 'trans_sd'

--- R/trans_sd.r
+++ R/trans_sd.r
@@ -5,7 +5,7 @@
 #' @param ref reference vector
 #' @references LIND, Marcus, et al. Variability of INR and its relationship with mortality, stroke, bleeding and hospitalisations in patients with atrial fibrillation. Thrombosis research, 2012, 129.1: 32-35.
 #' @examples
-#' # The code is equivalent to:
+#' # The code for trans_sd is equivalent to:
 #' # trans_sd <- function(x, y, ref){
 #' #    transformation_function <- function(y, ref) qnorm(ecdf(x=ref)(v=y))
 #' #    transformed_y <- transformation_function(y=y, ref=ref)
@@ -14,4 +14,21 @@
 #' # }
 #' @export
 
-trans_sd <- function(x, y, ref) sd(lm(qnorm(ecdf(ref)(y))~x)$residuals)
+trans_sd <- function(x, y, ref)
+    sd(x = lm(qnorm(p = ecdf(ref)(y)) ~ x)$residuals)
+
+
+#, @describeIn trans_sd return a function that has calculated the transformation
+#,   function already
+#, @export
+
+#' @rdname trans_sd
+#' @return for \code{trans_sd_fnc} a function of \code{x, y} where the
+#'   transformation function has already been calculated (unclear if this is
+#'   more efficient)
+#' @export
+
+trans_sd_fnc <- function(ref){
+    trans <- function(z) qnorm(p = ecdf(ref)(z))
+    function(x, y) sd(x = lm(trans(y) ~ x)$residuals)
+}
