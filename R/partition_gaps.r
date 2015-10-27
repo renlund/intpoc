0e654fcb2a9ec090d8e5f08af37185f2bf03703b
Henrik Renlund <henrikrenlund@gmail.com>
2015-10-27 10:41:07 +0100

    add 'partition_tir'

--- R/partition_gaps.r
+++ R/partition_gaps.r
@@ -11,7 +11,7 @@
 #' @return A list of \code{tbl_df} ('data_frame') objects
 #' @export
 
-partition_gaps <- function(x, y, gap=5, check=TRUE, mess=FALSE){
+partition_gaps <- function(x, y, gap=90, check=TRUE, mess=FALSE){
     if(check) {
         if(typeof(df <- check_num_vec(x, y, pre_mess = "[partition_gaps]", mess=mess))!="logical"){
             if(!is.null(df)){
@@ -38,4 +38,36 @@ partition_gaps <- function(x, y, gap=5, check=TRUE, mess=FALSE){
             y = y
             ))
     }
+}
+
+#' @description \code{partion_tir}: apply \code{tir} to each part of \code{partition_gaps}
+#' @describeIn partition_gaps
+#' @param low.tol the minimum number of points in each partition for proceeding
+#'    with tir calculation
+#' @param ... arguments passed to \code{tir}
+#' @note \code{part_tir} is very BETA!
+#' @export
+
+partition_tir <- function(x, y, gap = 90, check=TRUE, low.tol = 3, ...){
+    g <- partition_gaps(x = x, y = y, gap = gap, mess = TRUE)
+    gi <- lapply(g, function(x) nrow(x)>= low.tol)
+    g <- g[unlist(gi)]
+    fnc <- function(X) tir(x = X$x,  y = X$y, ...)
+#                            y.int = c(-Inf,2,3,Inf),
+#                            x.int = c(-Inf, Inf),
+#                            y.NA = "a",
+#                            group = TRUE,
+#                            incl.low = FALSE,
+#                            par.test = TRUE)
+    tg <- lapply(g, fnc)
+    RET <- tg[[1]]
+    n <- length(tg)
+    if(n>1){
+        for(i in 2:n){ # i = 2
+            RET$Time <- RET$Time + tg[[i]]$Time
+            RET$TiR <- RET$TiR + tg[[i]]$TiR
+        }
+        RET$percent <- RET$TiR / RET$Time
+    }
+    RET
 }
--- README.md
+++ README.md
@@ -1,4 +1,6 @@
+[![Travis-CI Build Status](https://travis-ci.org/renlund/intpoc.svg?branch=master)](https://travis-ci.org/renlund/intpoc)
+
 intpoc
 ======
 Points are considered defining a piece-wise linear curve. For these curves this package can calculate 'Time in Range', mean and standard deviation. (These have a very specific use in a project I'm working on...  Not considered to be interesting in general.)
-[![Travis-CI Build Status](https://travis-ci.org/renlund/intpoc.svg?branch=master)](https://travis-ci.org/renlund/intpoc)
+
